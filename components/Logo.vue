8993fdf686fa4c075454585eebd8dd9b35dfd74b
Christoph Franke <krito@krito.de>
2018-07-12 16:16:53 +0200

    logo styles

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,3 +1,13 @@
 <template>
 	<img src="/philippkoestermenke.svg">
 </template>
+
+<style lang="scss" scoped>
+	img {
+		width: 349px;
+		height: auto;
+		position: absolute;
+		left: 67px;
+		bottom: 37px;
+	}
+</style>
